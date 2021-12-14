9adae43181e19dd29b520910de7422371ac301c8
snahmd <sanahmed7@gmail.com>
2021-12-14 23:49:09 +0100

    connecting rest api and frontend

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,22 +1,28 @@
 import React , {useState, useEffect} from 'react'
 import { Row, Col } from 'react-bootstrap'
+import { useDispatch, useSelector } from 'react-redux'
 import axios from 'axios'
 import Product from '../components/Product'
-
+import {listProducts} from '../actions/productActions'
 
 
 const HomeScreen = () => {
-    const [products, setProducts] = useState([])
+    const dispatch = useDispatch()
+    const productList = useSelector(state => state.productList)
+    const {error, loading, products} = productList
     useEffect(async() => {
-        const {data}=await axios.get('http://127.0.0.1:8000/api/products')
-        setProducts(data)
+        dispatch(listProducts)
     }, [])
+    console.log('productList')
     return(
         <div>
+
             <h1>
             Latest Products
             </h1>
             
+                
+
                 <Row>
                     {products.map(product => (
                         <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -8,7 +8,7 @@ const ProductScreen = ()=>{
     const {id}=useParams()
     const [product, setProduct] = useState([])
     useEffect(async() => {
-        const {data}=await axios.get(`http://127.0.0.1:8000/api/product/`+id)
+        const {data}=await axios.get(`/api/product/`+id)
         setProduct(data)
     }, [])
     return (
